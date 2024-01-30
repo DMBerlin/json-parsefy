c1427147a132b32859e693659bc860c37d6a037d
Daniel Marinho <contato@danielmarinho.dev>
2024-01-29 22:03:32 -0300

    feat: decorators test

--- src/decorators/json-flattener.decorator.spec.ts
+++ src/decorators/json-flattener.decorator.spec.ts
@@ -1,4 +1,4 @@
-import { JSONFlattener } from "./json-flattener.decorator";
+import { JSONFlattener } from "@decorators/json-flattener.decorator";
 import { plainToClass } from "class-transformer";
 
 class TestClass {
@@ -7,16 +7,11 @@ class TestClass {
 }
 
 describe("JSONFlattener Decorator", () => {
-  let jsonObject: Record<string, any>;
-  let validJSONString: string;
-  let messyJSONString: string;
-  let unMessifiedJSONString: string;
-
-  beforeAll(() => {
-    jsonObject = {
+  it("should work on json objects", () => {
+    const jsonObject = {
       place: {
         name: "place-name",
-        countr: "place-country",
+        country: "place-country",
       },
       something: "something",
       local: {
@@ -25,7 +20,15 @@ describe("JSONFlattener Decorator", () => {
         mix: [125, false, "true"],
       },
     };
-    validJSONString = JSON.stringify({
+
+    const testClass: TestClass = plainToClass(TestClass, {
+      jsonProperty: jsonObject,
+    });
+    expect(testClass.jsonProperty).toMatchObject(jsonObject);
+  });
+
+  it("should parse normal json string into object", async () => {
+    const jsonString = JSON.stringify({
       name: "user-name",
       email: "user-email",
       prop: {
@@ -34,9 +37,18 @@ describe("JSONFlattener Decorator", () => {
       },
       identity: "user-identity",
     });
-    messyJSONString =
+
+    const testClass: TestClass = plainToClass(TestClass, {
+      jsonProperty: jsonString,
+    });
+    expect(testClass.jsonProperty).toMatchObject(JSON.parse(jsonString));
+  });
+
+  it("should work on json strings with escaped characters", () => {
+    const escapedJsonString =
       '"{\\r\\n    \\"glossary\\": {\\r\\n        \\"title\\": \\"example glossary\\",\\r\\n        \\"GlossDiv\\": {\\r\\n            \\"title\\": \\"S\\",\\r\\n            \\"GlossList\\": {\\r\\n                \\"GlossEntry\\": {\\r\\n                    \\"ID\\": \\"SGML\\",\\r\\n                    \\"SortAs\\": \\"SGML\\",\\r\\n                    \\"GlossTerm\\": \\"Standard Generalized Markup Language\\",\\r\\n                    \\"Acronym\\": \\"SGML\\",\\r\\n                    \\"Abbrev\\": \\"ISO 8879:1986\\",\\r\\n                    \\"GlossDef\\": {\\r\\n                        \\"para\\": \\"A meta-markup language, used to create markup languages such as DocBook.\\",\\r\\n                        \\"GlossSeeAlso\\": [\\"GML\\", \\"XML\\"]\\r\\n                    },\\r\\n                    \\"GlossSee\\": \\"markup\\"\\r\\n                }\\r\\n            }\\r\\n        }\\r\\n    }\\r\\n}"';
-    unMessifiedJSONString = JSON.stringify({
+
+    const unescapedJsonString = {
       glossary: {
         title: "example glossary",
         GlossDiv: {
@@ -57,27 +69,12 @@ describe("JSONFlattener Decorator", () => {
           },
         },
       },
-    });
-  });
+    };
 
-  it("should work on valid json strings", async () => {
     const testClass: TestClass = plainToClass(TestClass, {
-      jsonProperty: validJSONString,
+      jsonProperty: escapedJsonString,
     });
-    expect(testClass.jsonProperty).toEqual(validJSONString);
-  });
 
-  it("should work on messy json strings", () => {
-    const testClass: TestClass = plainToClass(TestClass, {
-      jsonProperty: messyJSONString,
-    });
-    expect(testClass.jsonProperty).toEqual(unMessifiedJSONString);
-  });
-
-  it("should work on json objects", () => {
-    const testClass: TestClass = plainToClass(TestClass, {
-      jsonProperty: jsonObject,
-    });
-    expect(testClass.jsonProperty).toEqual(jsonObject);
+    expect(testClass.jsonProperty).toMatchObject(unescapedJsonString);
   });
 });
